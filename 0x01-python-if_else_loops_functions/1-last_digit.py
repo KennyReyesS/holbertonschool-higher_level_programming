0638800e7eebf1065b249b1c8b229175362d9712
KennyReyesS <kennyreyes2000@gmail.com>
2021-04-27 20:25:37 +0000

    case in negative or positive

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastnumber = int(str(number)[-1:])
-if lastnumber > 5:
-    value = "and is greater than 5"
-elif lastnumber == 0:
-    value = "and is 0"
-elif lastnumber < 6 and lastnumber != 0:
-    value = "and is less than 6 and not 0"
-print("Last digit of {} is {} {}".format(number, lastnumber, value))
+
+if number < 0:
+    last_number = number % -10
+else:
+    last_number = number % 10
+
+print("Last digit of {} is {} ".format(number, last_number), end="")
+if last_number > 5:
+    print("and is greater than 5")
+elif last_number == 0:
+    print("and is 0")
+elif last_number < 6 and last_number != 0:
+    print("and is less than 6 and not 0")
