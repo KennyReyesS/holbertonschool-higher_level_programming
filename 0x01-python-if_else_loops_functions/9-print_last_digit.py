ff43b3c3e55b4aa96a34ceb83d09fe4fcc15ff9d
KennyReyesS <kennyreyes2000@gmail.com>
2021-04-27 23:15:04 +0000

    9-print_last_digit.py Python script created

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+def print_last_digit(number):
+    if number < 0:
+        last_number = (number % -10) * -1
+    else:
+        last_number = number % 10
+    print("{}".format(last_number), end="")
+    return last_number
