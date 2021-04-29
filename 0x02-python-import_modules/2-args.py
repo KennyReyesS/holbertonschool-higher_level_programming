4f6c91effb52ac0ca77d767c048f65565b1aacab
KennyReyesS <kennyreyes2000@gmail.com>
2021-04-29 22:43:06 +0000

    : added in arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__":
     import sys
     n_arg = len(sys.argv) - 1
     if n_arg == 1:
-        print("{} argument.".format(n_arg))
+        print("{} argument:".format(n_arg))
     elif n_arg < 1:
         print("{} arguments.".format(n_arg))
     else:
-        print("{} arguments.".format(n_arg))
+        print("{} arguments:".format(n_arg))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
