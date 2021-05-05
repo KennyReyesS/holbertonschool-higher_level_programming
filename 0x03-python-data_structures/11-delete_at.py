2c1dfd9b6a6cbddcbf12c6198e3093b55b0cb20c
KennyReyesS <kennyreyes2000@gmail.com>
2021-05-05 00:33:58 +0000

    11-delete_at.py function created

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if (idx >= 0):
+        for i in range(len(my_list)):
+            if (i == idx):
+                my_list.pop(idx)
+        return(my_list)
+    else:
+        return(my_list)
