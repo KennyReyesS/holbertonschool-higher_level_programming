47d49e5283148d15010743762eba667081dac23c
KennyReyesS <kennyreyes2000@gmail.com>
2021-05-05 00:37:12 +0000

    del function added

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,7 +3,7 @@ def delete_at(my_list=[], idx=0):
     if (idx >= 0):
         for i in range(len(my_list)):
             if (i == idx):
-                my_list.pop(idx)
+                del my_list[idx]
         return(my_list)
     else:
         return(my_list)
