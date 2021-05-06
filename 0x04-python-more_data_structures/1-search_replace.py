be6f5f5f21a54a198466a2074fd3ad72fd0f2aeb
KennyReyesS <kennyreyes2000@gmail.com>
2021-05-06 19:15:25 +0000

    function in one line

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is None:
-        return None
-    else:
-        new_list = my_list.copy()
-        for i in new_list:
-            new_list[search - 1] = replace
-        return(new_list)
+    return [replace if i == search else i for i in my_list]
