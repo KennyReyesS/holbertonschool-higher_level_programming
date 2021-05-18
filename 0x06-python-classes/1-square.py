c2f34a7861d3229803a21d55dc0690b9b050a8b1
KennyReyesS <kennyreyes2000@gmail.com>
2021-05-18 22:52:14 +0000

    class reuploaded

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,12 +2,13 @@
 """ Square with size.
 
 Attributes:
-    size (int): size of the square
+size (int): size of the square.
 
 """
 
 
 class Square:
-"""creating private instance attribute (size)"""
+    """class square"""
     def __init__(self, size):
+        """making private instance attribute"""
         self.__size = size
