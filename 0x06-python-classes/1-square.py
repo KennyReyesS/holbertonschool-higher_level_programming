670321f2a8f9893bdac7ceedbeaf31c19e1d3612
KennyReyesS <kennyreyes2000@gmail.com>
2021-05-18 22:26:22 +0000

    docstring added

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Square with size.
 
-
 Attributes:
     size (int): size of the square
 
@@ -9,5 +8,6 @@ Attributes:
 
 
 class Square:
+"""creating private instance attribute (size)"""
     def __init__(self, size):
         self.__size = size
