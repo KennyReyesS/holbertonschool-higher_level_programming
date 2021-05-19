aed411c667436172f728629504ba881f2cfa9d64
KennyReyesS <kennyreyes2000@gmail.com>
2021-05-19 17:31:38 +0000

    Square class fixed private

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -55,6 +55,7 @@ class Square:
         """set condition if size is equal to 0"""
         if self.__size == 0:
             print()
+            return
         for i in range(self.__position[1]):
             print("")
         for j in range(self.__size):
