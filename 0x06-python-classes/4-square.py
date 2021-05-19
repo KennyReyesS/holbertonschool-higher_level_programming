5e9d8551f65f1c53fd64219711a9b0352d45d3c3
KennyReyesS <kennyreyes2000@gmail.com>
2021-05-19 01:22:30 +0000

    Square class created

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Square size.
+
+creating square with size and area.
+
+"""
+
+
+class Square:
+    """Initializes the Square data"""
+    def __init__(self, size=0):
+        """Making private instance attribute"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """returns square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """sets conditions for the value of the square"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """Get the area of a square"""
+    def area(self):
+        """Return the square area"""
+        return (self.__size * self.__size)
