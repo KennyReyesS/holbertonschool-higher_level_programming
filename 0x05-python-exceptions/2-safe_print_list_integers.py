f52449eeec26dcefaf5daa10d3e2b9743658af58
KennyReyesS <kennyreyes2000@gmail.com>
2021-05-20 00:05:08 +0000

    function fixed

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    sum = 0
     i = 0
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
-            i += 1
+            sum += 1
         except (ValueError, TypeError):
-            continue
+            pass
     print("")
-    return i
+    return sum
