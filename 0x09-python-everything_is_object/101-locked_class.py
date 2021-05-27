81e3a384af5a8f755b56d002095608159410a2f9
KennyReyesS <kennyreyes2000@gmail.com>
2021-05-27 01:31:08 +0000

    function created

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""LockedClass.
+prevents the user from dynamically creating new instance attributes,
+except if the new instance attribute is called
+"""
+
+
+class LockedClass:
+    """__slots__ attribute"""
+    __slots__ = ["firts_name"]
