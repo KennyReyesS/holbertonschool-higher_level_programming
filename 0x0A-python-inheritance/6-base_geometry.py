f171b211e66f67c20c3d2f8718e136f12c0a8b68
KennyReyesS <kennyreyes2000@gmail.com>
2021-06-01 22:44:39 +0000

    Area of BaseGeometry created

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """initializes area of BaseGeometry"""
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
