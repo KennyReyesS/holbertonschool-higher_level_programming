50040c7b98b0e6e0ce5de438940ef92558c67d59
KennyReyesS <kennyreyes2000@gmail.com>
2021-06-02 20:49:38 +0000

    function created

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Importing json.
+"""
+
+
+import json
+
+"""
+3-to_json_string.py module.
+"""
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
