171cb330f8e87c593ddbf3628889c42c182bbec9
KennyReyesS <kennyreyes2000@gmail.com>
2021-06-02 23:47:46 +0000

    function cretated

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Importing json...
+"""
+
+
+import json
+
+"""
+5-save_to_json_file.py module
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON representation"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(my_obj))
