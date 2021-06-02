75a01a3c5bb768ed625f21ecf69e3a0464af1fa9
KennyReyesS <kennyreyes2000@gmail.com>
2021-06-02 01:01:00 +0000

    class Square created

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """initializes the Square data"""
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Returns the Square area"""
+        return (self.__size * self.__size)
