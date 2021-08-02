cd49bd7c979e1b3feda7328bfab8a849b1b16e2a
KennyReyesS <kennyreyes2000@gmail.com>
2021-08-02 20:54:26 +0000

    semistandard fixed

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-let myArgs = process.argv.length
+const myArgs = process.argv.length;
 
 if (myArgs < 3) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (myArgs === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
