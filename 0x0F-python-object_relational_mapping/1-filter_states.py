d1b3b37a62fc9d8671006eac57db26e8fd055c20
KennyReyesS <kennyreyes2000@gmail.com>
2021-08-07 17:12:29 +0000

    like in uppercase

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     curs = db.cursor()
-    curs.execute("SELECT * from states WHERE name like 'N%' ORDER BY `id` ASC")
+    curs.execute("SELECT * from states WHERE name LIKE 'N%' ORDER BY `id` ASC")
     data = curs.fetchall()
     for row in data:
         print(row)
