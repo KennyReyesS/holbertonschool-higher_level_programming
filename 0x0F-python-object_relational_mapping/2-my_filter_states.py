b6b0477bf4eb298063589675b7574705844137ff
KennyReyesS <kennyreyes2000@gmail.com>
2021-08-09 16:39:25 +0000

    condition search existing record

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,10 +11,11 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     curs = db.cursor()
-    curs.execute("SELECT * from states WHERE name like '{}' COLLATE latin1_bin ORDER BY `id` ASC".
+    curs.execute("SELECT * from states WHERE name like '{}' ORDER BY `id` ASC".
                  format(argv[4]))
     data = curs.fetchall()
     for row in data:
-        print(row)
+        if row[1] == state:
+            print(row)
     curs.close()
     db.close()
