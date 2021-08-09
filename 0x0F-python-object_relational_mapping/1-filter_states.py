beaf37ccce24d9c97154c21fcfec63324ffbce4f
KennyReyesS <kennyreyes2000@gmail.com>
2021-08-09 16:28:49 +0000

    COLLATE added for case sensirive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     curs = db.cursor()
-    curs.execute("SELECT * from states WHERE name LIKE 'N%' ORDER BY `id` ASC")
+    curs.execute("SELECT * FROM states WHERE name LIKE 'N%' \
+    COLLATE latin1_bin ORDER BY `id` ASC")
     data = curs.fetchall()
     for row in data:
         print(row)
