d03c614f8fffb306e83f65d4a41901df202bdda8
KennyReyesS <kennyreyes2000@gmail.com>
2021-08-20 02:42:49 +0000

    usr/bin changed

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
-#!/usr/bin
+#!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the responsel
 curl -s -X POST -H "Content-Type: application/json" -d @"$2" "$1"
