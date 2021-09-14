410ae27a86fdd2b22257ba308b067d346de125ab
KennyReyesS <kennyreyes2000@gmail.com>
2021-09-14 23:59:17 +0000

    try catch added

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,19 +5,23 @@ const myArgs = process.argv.slice(1);
 const url = myArgs[1];
 
 request(url, function (error, response, body) {
-  const respJson = JSON.parse(body);
-  if (error) {
-    console.log(error);
-  } else {
-    let n = 0;
-    for (const i in respJson.results) {
-      const characters = respJson.results[i].characters;
-      for (const urlid in characters) {
-        if (characters[urlid] === 'https://swapi-api.hbtn.io/api/people/18/') {
-          n++;
+  try {
+    const respJson = JSON.parse(body);
+    if (error) {
+      console.log(error);
+    } else {
+      let n = 0;
+      for (const i in respJson.results) {
+        const characters = respJson.results[i].characters;
+        for (const urlid in characters) {
+          if (characters[urlid] === 'https://swapi-api.hbtn.io/api/people/18/') {
+            n++;
+          }
         }
       }
+      console.log(n);
     }
-    console.log(n);
+  } catch (error) {
+      console.error(error);
   }
 });
