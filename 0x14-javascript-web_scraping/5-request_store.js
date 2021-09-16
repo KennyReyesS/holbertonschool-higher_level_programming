d6cc9367acb3fc59edadc0aeae5590382ad139ef
KennyReyesS <kennyreyes2000@gmail.com>
2021-09-16 00:52:35 +0000

    script fixed

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -2,15 +2,13 @@
 
 const request = require('request');
 const myArgs = process.argv.slice(2);
-const url = myArgs[2];
-const pathFile = myArgs[3];
 const fs = require('fs');
 
-request(url, function (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
-    fs.writeFile(pathFile, body, function (err) {
+    fs.writeFile(process.argv[3], body, 'utf8', function (err) {
       if (err) {
         return console.log(err);
       }
