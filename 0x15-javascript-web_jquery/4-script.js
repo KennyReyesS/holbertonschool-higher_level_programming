8d000ca72aa78b1be0bc591e8caaba443d70d1b9
KennyReyesS <kennyreyes2000@gmail.com>
2021-09-23 03:53:13 +0000

    script created

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,4 @@
+// Toggles the class of the <header> element when the user clicks on the tag DIV#toggle_header:
+$('#toggle_header').click(function (event) {
+  $('header').toggleClass('green red');
+});
