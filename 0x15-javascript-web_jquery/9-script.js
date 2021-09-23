d8af2aab2f576218d23d1eaa25270cdbceb59c0c
KennyReyesS <kennyreyes2000@gmail.com>
2021-09-23 06:43:32 +0000

    text added

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,1 +1,5 @@
 // Fetches from https://fourtonfish.com/hellosalut/?lang=fr and displays the value of hello from that fetch in the HTML tag DIV#hello.
+$.getJSON('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+// The translation  must be displayed in the HTML tag DIV#hello.
+  $('DIV#hello').text(data.hello);
+});
