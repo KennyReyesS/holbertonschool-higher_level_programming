fc4c72a8cc53cf17da729af69e55b669fcfca689
KennyReyesS <kennyreyes2000@gmail.com>
2021-09-23 06:37:19 +0000

    script created

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,1 +1,7 @@
 // Fetches and lists the title for all movies by using this URL: https://swapi-api.hbtn.io/api/films/?format=json.
+$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+// All movie titles must be list in the HTML tag UL#list_movies.
+  $.each(data.results, function (i, results) {
+    $('UL#list_movies').append('<li>' + results.title + '</li>');
+  });
+});
