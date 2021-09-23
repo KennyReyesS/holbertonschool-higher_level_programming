bb2428ca46aed0856dc8f86eb47ace381123c556
KennyReyesS <kennyreyes2000@gmail.com>
2021-09-23 06:17:00 +0000

    output fixed

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,1 +1,5 @@
 // Fetches the character name from this URL: https://swapi-api.hbtn.io/api/people/5/?format=json.
+$.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function (char) {
+// The name must be displayed in the HTML tag DIV#character.
+  $('DIV#character').text(char.name);
+});
